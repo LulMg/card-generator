7c201a3c88f39f75c694cbe9c9762c52f550aa76
LulMg <lulumarin.25.lm@gmail.com>
2022-03-30 19:32:46 +0000

    card-generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let iconos = ["♥", "♠", "♣", "♦"];
+let rCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function cartasGenerator(arr1, arr2) {
+  //CARTAS
+
+  let carta = document.getElementById("num");
+  let nroTarj = Math.floor(Math.random() * rCard.length);
+  let num = rCard[nroTarj];
+  let numero = (carta.innerHTML = num);
+
+  //ICONOS
+
+  let iconPaloS = document.querySelector(".icoSuperior");
+  let iconPaloI = document.querySelector(".icoInferior");
+
+  let palo = Math.floor(Math.random() * iconos.length);
+  let ico = iconos[palo];
+  let figura1 = (iconPaloS.innerHTML = ico);
+  let figura2 = (iconPaloI.innerHTML = ico);
+
+  // COLOR
+  for (let i of ico) {
+    if (i == "♥" || i == "♦") {
+      iconPaloI.style.color = "red";
+      iconPaloS.style.color = "red";
+      carta.style.color = "red";
+    } else {
+      iconPaloI.style.color = "black";
+      iconPaloS.style.color = "black";
+      carta.style.color = "black";
+    }
+  }
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("btn").addEventListener("click", () => {
+    cartasGenerator();
+  });
 };
